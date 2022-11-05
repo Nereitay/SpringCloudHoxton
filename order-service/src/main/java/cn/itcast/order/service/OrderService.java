e8c140abad00d2c03cd43c3cf1244d9403a59540
nerea <bonjourping@gmail.com>
2022-11-06 00:12:13 +0100

    Feign essential

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package cn.itcast.order.service;
 
+import cn.itcast.order.clients.UserClient;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.pojo.User;
@@ -14,6 +15,20 @@ public class OrderService {
     private OrderMapper orderMapper;
 
     @Autowired
+    private UserClient userClient;
+
+    public Order queryOrderById(Long orderId) {
+        // 1. 查询订单
+        Order order = orderMapper.findById(orderId);
+        // 2. 用Feign远程调用
+        User user = userClient.findById(order.getUserId());
+        // 3. 封装user到order
+        order.setUser(user);
+        // 4. 返回
+        return order;
+    }
+
+    /*@Autowired
     private RestTemplate restTemplate;
 
     public Order queryOrderById(Long orderId) {
@@ -29,5 +44,5 @@ public class OrderService {
         order.setUser(user);
         // 4.返回
         return order;
-    }
+    }*/
 }
